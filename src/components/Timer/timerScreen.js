c14caf3417fc80f310dbec3917bb95e3c3c87ce0
Gustavo <gustavooki.dev@gmail.com>
2024-09-20 13:52:22 -0300

    Update

--- src/components/Timer/timerScreen.js
+++ src/components/Timer/timerScreen.js
@@ -1,21 +1,6 @@
 import './timerScreen.css'
 
 const Timer = () => {
-    useEffect(() => {
-        
-
-        document.addEventListener('DOMContentLoaded', () => {
-        const timerDiv = document.getElementById('timer');
-        const stopDiv = document.getElementById('stop');
-
-        timerDiv.addEventListener('click', () => {
-            isRunning ? pauseTimer() : startTimer();
-        });
-
-        stopDiv.addEventListener('click', resetTimer);
-});
-    })
-
     return (
         <div className="timer" id="timer">
             <p id="timer-display">00:00</p>
